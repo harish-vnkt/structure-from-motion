395d4474db2fe063bf71e314285662f3c0320109
Harish238 <harishvenkat238@gmail.com>
2020-06-29 14:42:29 -0500

    Incorporate comments, logging and exception handling

--- view.py
+++ view.py
@@ -4,29 +4,30 @@ import pickle
 import cv2
 import numpy as np
 import glob
+import logging
 
 
 class View:
+    """Represents an image used in the reconstruction"""
 
     def __init__(self, image_path, root_path, feature_path, feature_type='sift'):
 
-        self.name = image_path[image_path.rfind('/') + 1:-4]
-        self.image = cv2.imread(image_path)
-        self.keypoints = []
-        self.descriptors = []
-        self.feature_type = feature_type
-        self.root_path = root_path
-        self.R = np.zeros((3, 3), dtype=float)
-        self.t = np.zeros((3, 1), dtype=float)
+        self.name = image_path[image_path.rfind('/') + 1:-4]  # image name without extension
+        self.image = cv2.imread(image_path)  # numpy array of the image
+        self.keypoints = []  # list of keypoints obtained from feature extraction
+        self.descriptors = []  # list of descriptors obtained from feature extraction
+        self.feature_type = feature_type  # feature extraction method
+        self.root_path = root_path  # root directory containing the image folder
+        self.R = np.zeros((3, 3), dtype=float)  # rotation matrix for the view
+        self.t = np.zeros((3, 1), dtype=float)  # translation vector for the view
 
         if not feature_path:
             self.extract_features()
         else:
             self.read_features()
 
-        self.point_map = {}
-
     def extract_features(self):
+        """Extracts features from the image"""
 
         if self.feature_type == 'sift':
             detector = cv2.xfeatures2d.SIFT_create()
@@ -35,31 +36,41 @@ class View:
         elif self.feature_type == 'orb':
             detector = cv2.ORB_create(nfeatures=1500)
         else:
-            print('Admitted values for the feature detector are: sift, surf or orb ')
+            logging.error("Admitted feature types are SIFT, SURF or ORB")
             sys.exit(0)
 
         self.keypoints, self.descriptors = detector.detectAndCompute(self.image, None)
+        logging.info("Computed features for image %s", self.name)
 
         self.write_features()
 
     def read_features(self):
+        """Reads features stored in files. Feature files have filenames corresponding to image names without extensions"""
 
-        features = pickle.load(open(self.root_path + '/features/' + self.name + '.pkl', "rb"))
+        # logic to compute features for images that don't have pkl files
+        try:
+            features = pickle.load(open(self.root_path + '/features/' + self.name + '.pkl', "rb"))
+            logging.info("Read features from file for image %s", self.name)
 
-        keypoints = []
-        descriptors = []
+            keypoints = []
+            descriptors = []
 
-        for point in features:
-            keypoint = cv2.KeyPoint(x=point[0][0], y=point[0][1], _size=point[1], _angle=point[2],
-                                    _response=point[3], _octave=point[4], _class_id=point[5])
-            descriptor = point[6]
-            keypoints.append(keypoint)
-            descriptors.append(descriptor)
+            for point in features:
+                keypoint = cv2.KeyPoint(x=point[0][0], y=point[0][1], _size=point[1], _angle=point[2],
+                                        _response=point[3], _octave=point[4], _class_id=point[5])
+                descriptor = point[6]
+                keypoints.append(keypoint)
+                descriptors.append(descriptor)
 
-        self.keypoints = keypoints
-        self.descriptors = np.array(descriptors)
+            self.keypoints = keypoints
+            self.descriptors = np.array(descriptors)  # convert descriptors into n x 128 numpy array
+
+        except FileNotFoundError:
+            logging.error("Pkl file not found for image %s. Computing from scratch", self.name)
+            self.extract_features()
 
     def write_features(self):
+        """Stores computed features to pkl files. The files are written inside a features directory inside the root directory"""
 
         if not os.path.exists(self.root_path + '/features'):
             os.makedirs(self.root_path + '/features')
@@ -76,14 +87,18 @@ class View:
 
 
 def create_views(root_path, image_format='jpg'):
+    """Loops through the images and creates an array of views"""
 
     feature_path = False
 
+    # if features directory exists, the feature files are read from there
+    logging.info("Created features directory")
     if os.path.exists(root_path + '/features'):
         feature_path = True
 
     image_names = sorted(glob.glob(root_path + '/images/*.' + image_format))
 
+    logging.info("Computing features")
     views = []
     for image_name in image_names:
         views.append(View(image_name, root_path, feature_path=feature_path))
